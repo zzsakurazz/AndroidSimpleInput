0e06b523f113efb34a9e1312fd3231f072a1dcf1
张铮 <zhangzheng@olayc.cn>
2019-07-05 11:12:30 +0800

    添加闪屏页

---
+++ app/src/main/java/com/sakura/simpleinput/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.sakura.simpleinput;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author zhangzheng
+ * @Date 2019-07-05 10:25
+ * @ClassName SplashActivity
+ * <p>
+ * Desc :
+ */
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
