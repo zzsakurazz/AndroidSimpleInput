70d00b562a6fc7887f865670584da9e20a91b9d8
张铮 <zhangzheng@olayc.cn>
2019-07-05 11:12:43 +0800

    添加帮助和说明页面UI

--- app/src/main/java/com/sakura/simpleinput/ExplainActivity.java
+++ app/src/main/java/com/sakura/simpleinput/ExplainActivity.java
@@ -3,6 +3,8 @@ package com.sakura.simpleinput;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * @author zhangzheng
@@ -13,10 +15,18 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class ExplainActivity extends AppCompatActivity {
 
+    private ImageView mBackIv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explain);
+        mBackIv = findViewById(R.id.explain_title_icon_back_iv);
+        mBackIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
