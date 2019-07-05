a3fc053f2bd111e39823a386d9fce6a3c3fc68ae
张铮 <zhangzheng@olayc.cn>
2019-07-05 09:56:00 +0800

    为首页标题添加字体

---
+++ app/src/main/java/com/sakura/simpleinput/ExplainActivity.java
@@ -0,0 +1,22 @@
+package com.sakura.simpleinput;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author zhangzheng
+ * @Date 2019-07-04 17:38
+ * @ClassName ExplainActivity
+ * <p>
+ * Desc :
+ */
+public class ExplainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explain);
+    }
+}
--- app/src/main/java/com/sakura/simpleinput/MainActivity.java
+++ app/src/main/java/com/sakura/simpleinput/MainActivity.java
@@ -1,13 +1,40 @@
 package com.sakura.simpleinput;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout mSettingLayout;
+    private ConstraintLayout mExplainLayout;
+    private TextView mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSettingLayout = findViewById(R.id.main_input_method_setting_layout);
+        mExplainLayout = findViewById(R.id.main_input_method_explain_layout);
+        mTitle = findViewById(R.id.main_input_method_title_tv);
+        Typeface typeFace = Typeface.createFromAsset(getAssets(), "font.ttf");
+        mTitle.setTypeface(typeFace);
+        mSettingLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mExplainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExplainActivity.class));
+            }
+        });
     }
 }
